15edaa623f57066c39104b43b6c026a54dc75158
giuliagolesano <giulia.golesano@studio.unibo.it>
2024-11-18 19:28:17 +0100

    display and libreries added

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <avr/sleep.h>
+#include <TimerOne.h>
+#include <EnableInterrupt.h>
+#include <LiquidCrystal_I2C.h>
 
 #include "Button.h"
 #include "Led.h"
@@ -12,17 +16,21 @@
 
 Button openButton(2);
 Button closeButton(3);
-Led greenLed(9);
-Led redLed(10);
+Led greenLed(10);
+Led redLed(9);
 ServoMotor door(8);
 Pir pir(7);
-WasteDetector wastedet(6,7);
+WasteDetector wastedet(5,6);
 Temp temp(A0);
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,20,4);
+
+unsigned long startTime;
 
 void setup() {
   // put your setup code here, to run once:
   //int result = myFunction(2, 3);
   Serial.begin(9600);
+
   openButton.begin();
   closeButton.begin();
   greenLed.begin();
@@ -31,6 +39,19 @@ void setup() {
   pir.begin();
   wastedet.begin();
   temp.begin();
+
+  //enableInterrupt(openButton, wakeUp, RISING);
+  //enableInterrupt(closeButton, wakeUp, RISING);
+
+  startTime = millis();
+
+  lcd.init();
+  lcd.backlight();
+  lcd.begin(20,4);
+
+  //Timer1.initialize(1000000);
+  //Timer1.attachInterrupt(ledManagement);
+  
 }
 
 void loop() {
