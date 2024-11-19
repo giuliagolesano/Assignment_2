4beae31bc7f64f7e7dddd2c808ba1da9ec63f985
giuliagolesano <giulia.golesano@studio.unibo.it>
2024-11-19 11:09:36 +0100

    implemented the State's enum

--- src/ServoMotor.cpp
+++ src/ServoMotor.cpp
@@ -1,26 +1,80 @@
 #include "ServoMotor.h"
 
+/*
+* Constructor.
+*/
 ServoMotor::ServoMotor(int p) {
     pin=p;
+    currentState = DOOR_CLOSED;
 }
 
+/*
+* Method to initialize the servo motor.
+*/
 void ServoMotor::begin() {
     servo.attach(pin);
     setAngle(0);
 }
 
+/*
+* Method to set the angle of the servo motor.
+*/
 void ServoMotor::setAngle(int angle) {
     servo.write(angle);
 }
 
+/*
+* Method to open the door.
+*/
 void ServoMotor::open() {
-    setAngle(90);
+    if(currentState == DOOR_CLOSED || currentState == DOOR_CLOSING){
+        currentState = DOOR_OPENING;    
+        setAngle(90);
+        currentState = DOOR_OPEN;
+    }
+
 }
 
+/*
+* Method to close the door.
+*/
 void ServoMotor::close() {
-    setAngle(0);
+    if(currentState == DOOR_OPEN || currentState == DOOR_OPENING){
+        currentState = DOOR_CLOSING;
+        setAngle(0);
+        currentState = DOOR_CLOSED;
+    }
 }
 
+/*
+* Method to reverse the door and empty the waste disposal system.
+*/
 void ServoMotor::reverse() {
-    setAngle(-90);
+    if(currentState == DOOR_CLOSED || currentState == DOOR_OPEN){
+        currentState = DOOR_REVERSING;
+        setAngle(-90);
+        delay(1000)
+        currentState = DOOR_CLOSED;
+    }
+}
+
+/*
+* Method to get the state.
+*/
+DoorState ServoMotor::getState() {
+    return currentState;
+}
+
+/*
+* Method to set the state.
+*/
+void ServoMotor::setState(DoorState newState) {
+    currentState = newState;
+}
+
+/*
+* Method to .
+*/
+void ServoMotor::update() {
+
 }
--- src/ServoMotor.h
+++ src/ServoMotor.h
@@ -4,15 +4,30 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+enum DoorState {
+    DOOR_CLOSED,
+    DOOR_OPENING,
+    DOOR_OPEN,
+    DOOR_CLOSING,
+    DOOR_REVERSING
+};
+
 class ServoMotor {
 
 private:
     Servo servo;
     int pin;
+    DoorState currentState;
 
 public:
     ServoMotor(int pin);
+
     void begin();
+
+    void update();
+    DoorState getState();
+    void setState(DoorState newState);
+
     void setAngle(int angle);
     void open();
     void close();
